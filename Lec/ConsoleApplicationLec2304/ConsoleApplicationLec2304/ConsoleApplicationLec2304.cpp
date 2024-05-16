a2fc83f212e97011cdf876586323ee73c431d3a9
Alexeycreator <rushev.alexey@gmail.com>
2024-05-16 14:19:49 +0300

    Лаба 7 сделана и отчет по ней

---
+++ Lec/ConsoleApplicationLec2304/ConsoleApplicationLec2304/ConsoleApplicationLec2304.cpp
@@ -0,0 +1,129 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node* next;
+};
+struct Stack {
+	Node* top;
+};
+void CheckTopNull(Stack& _stack, int value) {
+	if (_stack.top == NULL) {
+		Node* _node = new Node;
+		_node->data = value;
+		_node->next = NULL;
+		_stack.top = _node;
+	}
+}
+void Push(Stack& _stack, int value) {
+	Node* _node = new Node;
+	_node->data = value;
+	_node->next = NULL;
+	_stack.top = _node;
+}
+int PrintStack(Stack& _stack) {
+	if (_stack.top == NULL) {
+		cout << "Стэк пустой" << endl;
+		return -1;
+	}
+	int poppedValue = _stack.top->data;
+	if (_stack.top == _stack.top->next) {
+		delete _stack.top;
+		_stack.top = NULL;
+	}
+	else {
+		Node* temp = _stack.top;
+		_stack.top = _stack.top->next;
+		delete temp;
+	}
+	return poppedValue;
+}
+int main() {
+	setlocale(LC_ALL, "rus");
+	cout << "Лекция по ОП 23_04_2024" << endl;
+	Stack _stack;
+	_stack.top = NULL;
+	CheckTopNull(_stack, 5);
+	Push(_stack, 10);
+	Push(_stack, 15);
+	cout << PrintStack(_stack) << endl;
+	cout << PrintStack(_stack) << endl;
+	cout << PrintStack(_stack) << endl;
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//struct Node {
+//    int data;
+//    Node* next;
+//};
+//
+//struct Stack {
+//    Node* top;
+//};
+//
+//void checkTopNull(Stack& s, int value) {
+//    if (s.top == NULL) {
+//        Node* newNode = new Node;
+//        newNode->data = value;
+//        newNode->next = NULL;
+//        s.top = newNode;
+//    }
+//}
+//
+//void push(Stack& s, int value) {
+//    Node* newNode = new Node;
+//    newNode->data = value;
+//    newNode->next = s.top;
+//    s.top = newNode;
+//}
+//
+//int pop(Stack& s) {
+//    if (s.top == NULL) {
+//        cout << "пустой" << endl;
+//        return -1;
+//    }
+//
+//    int poppedValue = s.top->data;
+//    Node* temp = s.top;
+//    s.top = s.top->next;
+//    delete temp;
+//    return poppedValue;
+//}
+//
+//int main() {
+//    Stack s;
+//    s.top = NULL;
+//
+//    checkTopNull(s, 5);
+//
+//    push(s, 10);
+//    push(s, 15);
+//
+//    cout << pop(s) << endl; // Output should be 15
+//    cout << pop(s) << endl; // Output should be 10
+//    cout << pop(s) << endl; // Output should be 5
+//	  system("pause");
+//    return 0;
+//}
---
+++ SecondSemestr/ConsoleApplication7/ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,115 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+struct Q {
+    Node* h;
+    Node* t;
+};
+struct Stack
+{
+    Node* top;
+};
+void InputMassiv(int* arr, int n) {
+    for (int i = 0; i < n; i++) {
+        arr[i] = 1 + rand() % 50;
+    }
+}
+void PrintMassiv(int* arr, int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << "\t";
+    }
+    cout << endl;
+}
+void AddElem(Q &q, int value) {
+    Node* temp = new Node;
+    temp->data = value;
+    temp->next = NULL;
+    if (q.h == NULL) {
+        q.h = temp;
+        q.t = temp;
+    }
+    else {
+        q.t->next = temp;
+        q.t = q.t->next;
+    }
+}
+void Print(Node* h) {
+    Node* temp = h;
+    while (temp != NULL) {
+        cout << temp->data << "\t";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+void PrintStack(Node* stack) {
+    Node* temp = stack;
+    while (temp != NULL) {
+        cout << temp->data << "\t";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int Izvl(Q& q) {
+    Node* temp = q.h;
+    int a = temp->data;
+    q.h = temp->next;
+    delete temp;
+    return a;
+}
+bool GetSosiska(Q& q) {
+    if (q.h != NULL) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+void AddStack(Node*&stack, int value) {
+    Node* temp = new Node;
+    temp->data = value;
+    temp->next = NULL;
+    if (stack == NULL) {
+        stack = temp;
+    }
+    else{
+        temp->next = stack;
+        stack = temp;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    cout << "Лабораторная работа №7" << endl;
+    const int n = 5;
+    int arr[n];
+    InputMassiv(arr, n);
+    PrintMassiv(arr, n);
+    Q Q1 = {NULL, NULL};
+    Node* stack = NULL;
+    for (int i = 0; i < n; i++) {
+        AddElem(Q1, arr[i]);
+    }
+    Print(Q1.h);
+    cout << Izvl(Q1) << endl;
+    for (int i = 0; i < n; i++) {
+        if (GetSosiska(Q1) != false) {
+            cout << Izvl(Q1) << endl;
+        }
+        else {
+            cout << "Сосисок нет" << endl;
+        }
+    }
+    cout << "Стэк: " << endl;
+    for (int i = 0; i < n; i++) {
+        AddStack(stack, arr[i]);
+    }
+    PrintStack(stack);
+    system("pause");
+    return 0;
+}
